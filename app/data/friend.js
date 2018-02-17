57dc042a9b154f6dd4ecef667944a6408843f222
Earl Clarin <earl.clarin@gmail.com>
2018-02-16 20:52:19 -0800

    first commit

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'Earl Clarin',
+    profilePic: 'https://upload.wikimedia.org/wikipedia/en/a/a6/Goofy.svg',
+    scores: [
+      "4",
+      "4",
+      "3",
+      "2",
+      "2",
+      "3",
+      "4",
+      "4",
+      "5",
+      "1"
+    ]
+  }
+]
+
+module.exports = friendList;
